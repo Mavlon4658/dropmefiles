662904016e2fbb03cb2f3fbfc620756fe38062de
Mavlon4658 <mavlon4658@gmail.com>
2023-10-31 21:00:29 +0500

    html ready

--- src/main.js
+++ src/main.js
@@ -2,5 +2,18 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import { createI18n } from 'vue-i18n'
+import en from '@/lang/en.json';
+import ru from '@/lang/ru.json';
+import '@/assets/css/new.css'
 
-createApp(App).use(store).use(router).mount('#app')
+const t = createI18n({
+    locale: 'ru',
+    allowComposition: true,
+    messages: {
+        en: en,
+        ru: ru
+    }
+})
+
+createApp(App).use(t).use(store).use(router).mount('#app')
